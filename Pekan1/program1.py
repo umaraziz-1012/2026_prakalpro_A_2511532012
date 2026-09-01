d0ec250237a2c59b520e6231064f5d642524e8b9
Aziz <aziz@gmail.com>
2026-09-01 10:11:16 +0700

    Praktikum1 alpro selasa

---
+++ Pekan1/program1.py
@@ -0,0 +1,12 @@
+#Program pertama
+"""
+Komentar 
+dua baris
+atau lebih
+"""
+print("Hello world")
+print()
+print("program ini menghasilkan")
+print("empat baris output")
+print("/\\/\\\\///\\\\\\")
+print("program ini " + "dibuat oleh a\n" + "nama dan nim")
