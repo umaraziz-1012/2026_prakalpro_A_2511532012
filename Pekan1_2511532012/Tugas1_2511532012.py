3b7a49c92b98935caec8a7cb9535fca1c31332e9
umaraziz-1012 <umarabdaziz515@gmail.com>
2026-09-08 09:45:21 +0700

    Add files via upload

---
+++ Pekan1_2511532012/Tugas1_2511532012.py
@@ -0,0 +1,7 @@
+nama_2012 = "Aziz"
+tanggal_lahir_2012 = "10-12-2006"
+tempat_lahir_2012 = "Bukittinggi"
+
+print("perkenalkan nama saya adalah",nama_2012)
+print("tanggal lahir saya adalah",tanggal_lahir_2012)
+print("tempat lahir saya adalah",tempat_lahir_2012)
